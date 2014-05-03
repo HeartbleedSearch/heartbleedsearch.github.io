83049e58f374d105475b64bb90e8f52f321b70d5
talves <tony@3alves.com>
2014-05-03 08:42:55 -0700

    rate and add button

--- _files/js/options.js
+++ _files/js/options.js
@@ -5,3 +5,10 @@ $(document).ready(function(event) {
 $(window).on('hashchange', function() {
   //console.log("------------ hashchange -------------");
 });
+if (chrome.app.isInstalled) {
+  document.getElementById('install-button').style.display = 'none';
+  document.getElementById('rate-button').style.display = 'inline';
+} else {
+  document.getElementById('install-button').style.display = 'inline';
+  document.getElementById('rate-button').style.display = 'none';
+}
