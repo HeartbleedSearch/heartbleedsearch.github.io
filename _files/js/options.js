88ab68e59499a4b6c144aba5a9cae477a7bf1d64
talves <tony@3alves.com>
2014-05-06 11:24:15 -0700

    bump to 0.1.1

--- _files/js/options.js
+++ _files/js/options.js
@@ -1,15 +1,23 @@
 
 $(document).ready(function(event) {
   //console.log("------------ DOM loaded -------------");
+  checkForInstalledExt();
 });
 $(window).on('hashchange', function() {
   //console.log("------------ hashchange -------------");
 });
-var installedExt = document.getElementById('extension-is-installed');
-if (installedExt) {
-  document.getElementById('install-button').style.display = 'none';
-  document.getElementById('rate-button').style.display = 'inline';
-} else {
-  document.getElementById('install-button').style.display = 'inline';
-  document.getElementById('rate-button').style.display = 'none';
+function checkForInstalledExt() {
+  //console.log("------------ checkForInstalledExt -------------");
+  var installedExt = document.getElementById('extension-is-installed');
+  var installButton = document.getElementById('install-button');
+  var rateButton = document.getElementById('rate-button');
+  if (chrome && installButton !== undefined && rateButton !== undefined) {
+    if (installedExt === undefined) {
+      installButton.style.display = 'none';
+      rateButton.style.display = 'inline';
+    } else {
+      installButton.style.display = 'inline';
+      rateButton.style.display = 'none';
+    }
+  }
 }
